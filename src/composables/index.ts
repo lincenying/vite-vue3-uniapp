2fdaef6d6b02176b313e500dad405fcda8b98c6c
林岑影 <lincenying@qq.com>
2023-07-26 00:54:13 +0800

    chore: update

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -84,10 +84,45 @@ export function getPageHeight(isTab = false) {
 
 type ToastIocn = 'success' | 'loading' | 'error' | 'none' | 'fail' | 'exception'
 
+/**
+ * 封装uni.showToast
+ * @param title 标题
+ * @param icon 图标
+ * @param duration 延迟时间
+ */
 export function showToast(title: string, icon: ToastIocn = 'none', duration = 2000) {
     uni.showToast({
         title,
         icon,
         duration,
     })
+}
+
+/**
+ * 竞态锁
+ * @param fn 回调函数
+ * @param autoUnlock 是否自动解锁
+ * @returns void
+ * ```
+ * autoUnlock === true 不管 fn 返回什么, 都自动解锁
+ * autoUnlock === false 不管 fn 返回什么, 都不自动解锁
+ * autoUnlock === 'auto' 当 fn 返回 false 时, 不自动解锁, 返回其他值时, 自动解锁
+ * ```
+ */
+export function useLockFn(fn: AnyFn, autoUnlock: boolean | 'auto' = 'auto') {
+    const lock = ref(false)
+    return async (...args: any[]) => {
+        if (lock.value)
+            return
+        lock.value = true
+        try {
+            const $return: any = await fn(...args)
+            if (autoUnlock === true || (autoUnlock === 'auto' && $return !== false))
+                lock.value = false
+        }
+        catch (e) {
+            lock.value = false
+            throw e
+        }
+    }
 }
