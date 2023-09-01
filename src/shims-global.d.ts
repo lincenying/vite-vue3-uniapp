125dd86a3e3beec9c6b9c5ad4783ab2b90e9ee47
林岑影 <lincenying@qq.com>
2023-09-01 23:22:03 +0800

    chore: update

--- src/shims-global.d.ts
+++ src/shims-global.d.ts
@@ -1,11 +1,15 @@
 /**
- * Null 或者 T
+ * Null 或者 Undefined 或者 T
  */
-declare type Nullable<T> = T | null
+declare type Nullable<T> = T | null | undefined
 /**
- * Undefined 或者 T
+ * 非 Null 类型
  */
-declare type UnfAble<T> = T | undefined
+declare type NonNullable<T> = T extends null | undefined ? never : T
+/**
+ * 数组<T> 或者 T
+ */
+declare type Arrayable<T> = T | T[]
 /**
  * 键为字符串, 值为 Any 的对象
  */
@@ -14,8 +18,18 @@ declare type Obj = Record<string, any>
  * 键为字符串, 值为 T 的对象
  */
 declare type ObjT<T> = Record<string, T>
-
-declare type AnyFn = (...args: any[]) => any;
+/**
+ * Function
+ */
+declare type Fn<T = void> = () => T
+/**
+ * 任意函数
+ */
+declare type AnyFn = (...args: any[]) => any
+/**
+ * Promise, or maybe not
+ */
+declare type Awaitable<T> = T | PromiseLike<T>
 
 declare interface ResponseData<T> {
     data: T
